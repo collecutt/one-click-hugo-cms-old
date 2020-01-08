63f4d307e120f5a316eedb75ebea521419edca62
Squirtal <56996851+Squirtal@users.noreply.github.com>
2020-01-08 13:50:57 +0100

    Update cms.js

--- src/js/cms.js
+++ src/js/cms.js
@@ -17,6 +17,5 @@ CMS.registerPreviewTemplate("products", ProductsPreview);
 CMS.registerPreviewTemplate("values", ValuesPreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
 CMS.init();
-npm install --save netlify-cms-widget-youtube
 import youtube from "netlify-cms-widget-youtube";
 CMS.registerWidget("youtube", youtubeControl, youtubePreview);
