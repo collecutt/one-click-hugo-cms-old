c927a34ae175dbbed2e4d5d8c3f324b8701513a4
Squirtal <56996851+Squirtal@users.noreply.github.com>
2020-01-08 14:07:28 +0100

    Update cms.js

--- src/js/cms.js
+++ src/js/cms.js
@@ -1,6 +1,6 @@
 import React from "react";
 import CMS from "netlify-cms-app";
-
+import youtube from "netlify-cms-widget-youtube";
 // Import main site styles as a string to inject into the CMS preview pane
 import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
 
