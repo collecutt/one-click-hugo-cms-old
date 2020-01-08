2710380795aa8c01dedfe261898d14ab7b0dde50
Squirtal <56996851+Squirtal@users.noreply.github.com>
2020-01-08 14:02:08 +0100

    Update cms.js

--- src/js/cms.js
+++ src/js/cms.js
@@ -1,6 +1,10 @@
 import React from "react";
 import CMS from "netlify-cms-app";
-
+<script src="https://unpkg.com/netlify-cms-widget-youtube@^0.3.0"></script>
+ 
+<script>
+  CMS.registerWidget('youtube', youtubeControl, youtubePreview)
+</script> 
 // Import main site styles as a string to inject into the CMS preview pane
 import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
 
